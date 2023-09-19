69f419eb8e2b38de1aca0c19619126737e791212
onebarloop <alexanderbayer@posteo.de>
2023-09-19 18:48:40 +0200

    readable Stream

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
+import figlet from 'figlet';
+
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
+  async fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === '/')
-      return new Response(Bun.file(import.meta.dir + '/index.html'));
-    if (url.pathname === '/blog') return new Response('Blog!');
+    const body = figlet.textSync('Selly und Alex <3');
+
+    if (req.method === 'GET') {
+      if (url.pathname === '/')
+        return new Response(Bun.file(import.meta.dir + '/index.html'));
+      if (url.pathname === '/bun') return new Response(body);
+    }
+
+    if (req.method === 'POST') {
+      if (url.pathname === '/api') {
+        for await (const chunk of req.body!) {
+          const test = new TextDecoder().decode(new Uint8Array(chunk));
+          console.log(new TextDecoder().decode(new Uint8Array(chunk)));
+          return new Response(figlet.textSync(test));
+        }
+      }
+    }
 
     return new Response(`404!`);
   },
